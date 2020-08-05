5ec0ce63338d5e0a97c6d47ecc02d7d03f0f622e
asmagala <asmagala@op.pl>
2020-08-05 14:46:15 +0200

    Add processing websocket communication to server.js.

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
+
+let tasks = [];
+
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running...');
+});
+
+const io = seocket(server);
+
+io.on('connection', (socket) => {
+  console.log(`Added new connection with id = ${socket.id}`);
+  socket.emit('updateData', tasks);
+
+  socket.on('addTask', (task) => {
+    console.log('Added new task', task);
+    tasks.push(task);
+    socket.broadcast.emit('addTask', task);
+  });
+
+  socket.on('removeTask', (id) => {
+    console.log(`Removed task ${id}`);
+    tasks.splice(id, 1);
+    socket.broadcast.emit('removeTask', id);
+  });
 });
 
 app.use((req, res) => {
